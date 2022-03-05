c276cdb6279edad0d838db77ae0e83764ae33638
Kargos <>
2022-03-05 11:38:52 +0100

    Phase 2.2 : Mission 2 : Ajout de la liste des formations avec filtres sur la page d'administration

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,14 +14,38 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AdministrationController extends AbstractController{
     
+        /**
+     *
+     * @var FormationRepository
+     */
+    private $repository;
+    
+    /**
+     * 
+     * @param FormationRepository $repository
+     */
+    function __construct(FormationRepository $repository) {
+        $this->repository = $repository;
+    }
     
     /**
      * @Route("/administration", name="administration")
      * @return Response
      */
     public function index(): Response{
+        $formations = $this->repository->findAll();
+        return $this->render("pages/administration.html.twig", [
+            'formations' => $formations
+        ]);
+    }
+    
+    /**
+     * @Route("/administration", name="administration")
+     * @return Response
+     
+    public function index(): Response{
         return $this->render("pages/administration.html.twig", [
         ]);  
-    }
+    }*/
     
 }
