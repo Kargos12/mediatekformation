18ccae26d867d45400204cc997961ae243ebeb6c
Kargos <>
2022-03-05 15:39:05 +0100

    Phase 2.3 : Mission 2 : Création du bouton suppression

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Formation;
 use App\Repository\FormationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,8 +26,9 @@ class AdministrationController extends AbstractController{
      * 
      * @param FormationRepository $repository
      */
-    function __construct(FormationRepository $repository) {
+    function __construct(FormationRepository $repository,EntityManagerInterface $om) {
         $this->repository = $repository;
+        $this->om = $om;
     }
     
     /**
@@ -38,14 +41,51 @@ class AdministrationController extends AbstractController{
             'formations' => $formations
         ]);
     }
+     /**
+     * @Route("/administration/tri/{champ}/{ordre}", name="administration.sort")
+     * @param type $champ
+     * @param type $ordre
+     * @return Response
+     */
+    public function sort($champ, $ordre): Response{
+        $formations = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/administration.html.twig", [
+            'formations' => $formations
+            ]);
+    }   
+    
+     /**
+     * @Route("/administration/recherche/{champ}", name="administration.findallcontain")
+     * @param type $champ
+     * @param Request $request
+     * @return Response
+     */
+    public function findAllContain($champ, Request $request): Response{
+        if($this->isCsrfTokenValid('filtre_'.$champ, $request->get('_token'))){
+            $valeur = $request->get("recherche");
+            $formations = $this->repository->findByContainValue($champ, $valeur);
+            return $this->render("pages/administration.html.twig", [
+            'formations' => $formations
+            ]);
+        }
+        return $this->redirectToRoute("formations");
+    }  
     
     /**
-     * @Route("/administration", name="administration")
-     * @return Response
-     
-    public function index(): Response{
-        return $this->render("pages/administration.html.twig", [
-        ]);  
-    }*/
+     * 
+     * @var EntityManagerInterface
+     */
+    private $om;
     
+    /**
+     * Suppression d'une formation
+     * @Route("administration/suppr/{id}", name="administration.suppr")
+     * @param Formation $formation
+     * @return Response
+     */
+    public function suppr (Formation $formation) : Response {
+        $this->om->remove($formation);
+        $this->om->flush();
+        return $this->redirectToRoute("administration");
+    }
 }
